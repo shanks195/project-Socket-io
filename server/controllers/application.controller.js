22f5a0f7e879b5bbe5f2b90c8920e802b200d72e
shanks195 <robocon87@gmail.com>
2020-11-12 16:50:48 +0700

    update

--- server/controllers/application.controller.js
+++
@@ -1,5 +0,0 @@
-//goi database ra tu file db.js
-var db = require("../db");
-module.exports.pokedex = function(req, res) {
-    res.render("web/application/pokedex/index");
-}
--- server/public/js/IndexSki.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("#loginForm").show();
-    $('#Home_Content').hide();
-})
-var socket = io.connect('http://localhost:3000');
